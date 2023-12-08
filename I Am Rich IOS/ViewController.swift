37e581e72ad68f20ca409757ccdcd755e60a3cbc
matthew colson <matthew.p.colson@gmail.com>
2023-12-08 15:34:17 -0600

    Initial Commit

---
+++ I Am Rich IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich IOS
+//
+//  Created by Matthew Colson on 12/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
